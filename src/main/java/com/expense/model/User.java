e2e30dd9c9c4083cdd1fdf86699e0e8e9ba41e5f
blueskyleader01 <blueskyleader01@hotmail.com>
2021-09-24 11:06:42 -0400

    first commit

---
+++ src/main/java/com/expense/model/User.java
@@ -0,0 +1,57 @@
+package com.expense.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
